ad3dd1bccb1f6e0ce07a1fcb53ceb36c986c982b
Krzysiek Gonet <gonetkrzysiek@gmail.com>
2021-07-01 20:55:05 +0200

    Add base db models

--- nextgen/data/dbmodels.py
+++ nextgen/data/dbmodels.py
@@ -1,16 +1,38 @@
 from datetime import datetime
+from typing import Any
 
+from bson import ObjectId
 from mongoengine import Document
 from mongoengine.document import EmbeddedDocument
-from mongoengine.fields import BooleanField, DateTimeField, FloatField, IntField, ListField, ReferenceField, StringField
+from mongoengine.fields import (
+    BooleanField,
+    DateTimeField,
+    EmbeddedDocumentField,
+    FloatField,
+    IntField,
+    ListField,
+    ReferenceField,
+    StringField,
+)
+from pydantic.main import BaseModel
 
 from nextgen.data.decorators import custom_collection_name
 
 
 class BaseMongoDocument(Document):
-    created = DateTimeField(default=datetime.utcnow())
-    modified = DateTimeField(default=datetime.utcnow())
+    created = DateTimeField(default=datetime.utcnow().astimezone())
+    modified = DateTimeField(default=datetime.utcnow().astimezone())
     meta = {"abstract": True}
+
+    def to_dict(self) -> dict[str:Any]:
+        parsed = self.to_mongo().to_dict()
+        if "id" not in parsed:
+            parsed["id"] = parsed.pop("_id", None)
+        return parsed
+
+    @classmethod
+    def from_api_model(cls, api_model: BaseModel):
+        return cls(**api_model.dict())
 
 
 @custom_collection_name
@@ -22,34 +44,35 @@ class User(BaseMongoDocument):
 
 @custom_collection_name
 class EvoResultDetails(BaseMongoDocument):
-    mean: ListField
-    std: ListField
-    best_solutions: ListField
+    mean = ListField()
+    std = ListField()
+    best_solutions = ListField()
 
 
-class ResultDetils(EmbeddedDocument):
-    result_details_id: ReferenceField(EvoResultDetails)
-    mutation_probability: FloatField
-    mutation_points_number: IntField
-    mutation_type: StringField
-    crossover_probability: FloatField
-    crossover_points_number: IntField
-    crossover_type: StringField
-    selection_type: StringField
-    duel_groups: IntField
-    duel_group_size: IntField
-    best_percentage: IntField
-    use_inverse: BooleanField
-    inverse_probability: FloatField
-    use_elite: BooleanField
-    elite_percentage: IntField
-    population_size: IntField
-    tollerance: FloatField
+class ResultDetails(EmbeddedDocument):
+    result_details_id = ReferenceField(EvoResultDetails)
+    mutation_probability = FloatField()
+    mutation_points_number = IntField()
+    mutation_type = StringField()
+    crossover_probability = FloatField()
+    crossover_points_number = IntField()
+    crossover_type = StringField()
+    selection_type = StringField()
+    duel_groups = IntField()
+    duel_group_size = IntField()
+    best_percentage = IntField()
+    use_inverse = BooleanField()
+    inverse_probability = FloatField()
+    use_elite = BooleanField()
+    elite_percentage = IntField()
+    population_size = IntField()
+    tollerance = FloatField()
 
 
 @custom_collection_name
 class EvoResult(BaseMongoDocument):
-    user_id: ReferenceField(User)
-    generations: IntField
-    best_solution: ListField
-    elapsed_seconds: FloatField
+    user_id = ReferenceField(User)
+    generations = IntField()
+    best_solution = ListField()
+    elapsed_seconds = FloatField()
+    details = EmbeddedDocumentField(ResultDetails)
