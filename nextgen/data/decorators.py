329a08bdba96a1f2fb5a9d5f1c74a2cd6ef69124
Krzysiek Gonet <gonetkrzysiek@gmail.com>
2021-07-01 20:58:40 +0200

    Add mongo collection name decorator

--- nextgen/data/decorators.py
+++ nextgen/data/decorators.py
@@ -5,7 +5,7 @@ def custom_collection_name(cls):
     def wrap(cls):
         new_collection_name = re.sub(r"(?<!^)(?=[A-Z])", "_", cls.__name__).lower() + "s"
 
-        # It seems like mongoengine has an some issue with settings meta.collection inside child class so modify _meta attribute directly.
+        # It seems like mongoengine has an some issue with settings 'meta.collection' inside child class so modify '_meta' attribute directly.
         getattr(cls, "_meta")["collection"] = new_collection_name
         return cls
 
