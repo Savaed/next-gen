ab4333e08c8f38c5ea4b4cbea8a469ba4bd77c9f
Krzysiek Gonet <gonetkrzysiek@gmail.com>
2021-07-02 23:06:30 +0200

    Add template clientapp

---
+++ clientapp/src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+// import vuetify from "@/plugins/vuetify";
+import App from "@/App.vue";
+import router from "@/router";
+import store from "@/store";
+// import i18n from "@/plugins/i18n.js";
+
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+// app.use(vuetify);
+// app.use(i18n);
+
+app.mount("#app");
